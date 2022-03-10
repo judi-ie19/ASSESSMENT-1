0985f51894f0dee569b7d3af2a9feade4912354e
jud-ie19 <owuorakinyijudith@gmail.com>
2022-03-10 08:31:51 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    name("codehive")
+    var w= println(5%3)
+    fact()
+    Myname()
+
+
+}
+fun name(name:String){
+    var y= "codehive"
+    println(y[4].toString()+y[5]+y[6]+y[7])
+
+}
+fun modulus(a:Int,b:Int):Int{
+    var w= a%b
+    return w
+}
+fun fact(){
+    var p= "judith Akinyi"
+    var z= "21"
+    println("hi, my name is $p"+" "+" "+"and i am $z yaers old")
+}
+fun Myname(){
+    var r="love"
+    println(r.length)
+}
+
